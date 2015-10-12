af7bf5bb0e3d2c583357eaf3333dab24a8900da9
memetcircus <mehmetakifacar@gmail.com>
2015-10-12 12:17:12 +0300

    Initial Commit

---
+++ onthemap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  onthemap
+//
+//  Created by Mehmet Akif Acar on 12/10/15.
+//  Copyright © 2015 memetcircus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
